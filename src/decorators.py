7c09626593876737e2290f2936df60fa7050aaa0
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-01-07 03:21:51 +0800

    revision

--- src/decorators.py
+++ src/decorators.py
@@ -1,45 +1,50 @@
 from functools import wraps
 from logging import error
-from typing import Callable, Any
+from typing import Any, Callable
+
 
 # Создан декоратор log, который будет автоматически регистрировать детали выполнения функций
 def log(filename: str | None = None) -> Callable:
-  def decorator(func: Callable) -> Callable:
-    @wraps(func)
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
-      try:
-        # После выполненя функции будет выводиться сообщение, которую будешь записывать в файл
-        func(*args, **kwargs)
-        message_for_log = f"{func.__name__} ok\n"
-        if filename: # если задан параметр filename
-          with open(filename, 'w', encoding="utf-8") as file:
-            file.write(message_for_log)
-        else: # если не задан параметр filename
-            print(message_for_log)
-      except Exception as e:
-        # выполняешь действие если ошибка и записывай в log_message сообщение, которое будешь распечатывать
-        error_message = f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}\n"
-        if filename: # если задан параметр filename
-          with open(filename, "a", encoding="utf-8") as file:
-            file.write(error_message)
-        else:
-          print(error_message)
-      return error
-    return wrapper
-  return decorator
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                # После выполнения функции сообщение будет выводиться в терминал или записано в файл
+                func(*args, **kwargs)
+                message_for_log = f"{func.__name__} ok\n"
+                if filename:  # если задан параметр filename
+                    with open(filename, "w", encoding="utf-8") as file:
+                        file.write(message_for_log)
+                else:  # если не задан параметр filename
+                    print(message_for_log)
+            except Exception as e:
+                # выполняешь действие, если ошибка, записывается в файл или выводиться в консоли
+                error_message = f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}\n"
+                if filename:  # если задан параметр filename
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(error_message)
+                else:
+                    print(error_message)
+            return error
+
+        return wrapper
+
+    return decorator
 
 
 # проверка декоратора
 @log(filename="mylog.txt")
-def my_function(x, y):
-  print(f"Выполнение функции с аргументами {x} и {y}")
+def my_function_1(x: Any, y: Any) -> None:
+    print(f"Выполнение функции с аргументами {x} и {y}")
 
-my_function(10, 20)
+
+my_function_1(10, 20)
 
 
 @log(filename="mylog.txt")
-def my_function(x, y):
+def my_function_2(x: Any, y: Any) -> None:
     return x / y
 
-my_function("1", "9")
-print(my_function)
+
+my_function_2("1", "9")
+print(my_function_2)
