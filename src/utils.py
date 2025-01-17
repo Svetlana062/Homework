986b473ae940a20249e70f37342579d752196e21
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-01-18 03:39:02 +0800

    add corrections

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ def read_file(path: str) -> list[dict]:
     """Функция, которая принимает на вход путь до JSON-файла и
     возвращает список словарей с данными о финансовых транзакциях"""
     try:
-        with open(path, 'r', encoding='utf-8') as f:
+        with open(path, "r", encoding="utf-8") as f:
             try:
                 data = json.load(f)
             except json.JSONDecodeError:
@@ -16,4 +16,5 @@ def read_file(path: str) -> list[dict]:
         print("Файл не найден")
         return []
 
-print(read_file('./data/operations.json'))
+
+print(read_file("./data/operations.json"))
