166007f98aac780b90438e176162ce8c49a3e6a3
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-01-18 00:02:02 +0800

    add utils.py, operations.json, sample .env.example

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import json
+
+
+def read_file(path: str) -> list[dict]:
+    """Функция, которая принимает на вход путь до JSON-файла и
+    возвращает список словарей с данными о финансовых транзакциях"""
+    try:
+        with open(path, 'r', encoding='utf-8') as f:
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                print("Ошибка при декодировании")
+                return []
+        return data
+    except FileNotFoundError:
+        print("Файл не найден")
+        return []
+
+print(read_file('./data/operations.json'))
