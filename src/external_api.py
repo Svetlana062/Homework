576ae7936b69f105f10d8b863701fb28b31f86c5
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-01-23 02:29:01 +0800

    Created a separate logger object for modules: masks, utils

--- src/external_api.py
+++ src/external_api.py
@@ -30,16 +30,10 @@ if __name__ == "__main__":
                 "id": 214024827,
                 "state": "EXECUTED",
                 "date": "2018-12-20T16:43:26.929246",
-                "operationAmount": {
-                    "amount": "70946.18",
-                    "currency": {
-                        "name": "USD",
-                        "code": "USD"
-                    }
-                },
+                "operationAmount": {"amount": "70946.18", "currency": {"name": "USD", "code": "USD"}},
                 "description": "Перевод организации",
                 "from": "Счет 10848359769870775355",
-                "to": "Счет 21969751544412966366"
+                "to": "Счет 21969751544412966366",
             },
         )
     )
